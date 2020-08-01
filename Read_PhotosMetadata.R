cd191bac861e5d3b01e5d194297e9a244d672b86
Gonzalo Bravo <gonzalobravoargentina@gmail.com>
2020-07-31 21:34:59 -0300

    Update Read_PhotosMetadata.R

--- Read_PhotosMetadata.R
+++ Read_PhotosMetadata.R
@@ -1,5 +1,6 @@
 
-# Set the workin directory d to the folder with the photos (in r Studio= session/Set Working directory /Choose directory)
+# Set working directory to the folder with the photos (in r Studio= session/Set Working directory /Choose directory)
+#For this example use photos folder as wd
 files <- list.files(pattern = "*.jpg")#get a list of files .jpg in wd
 
 library(exifr)
@@ -8,6 +9,7 @@ dat <- as.data.frame(dat)# create a dataframe
 #create a short data.frame with the columms of interest 
 library(dplyr)
 #names used as MBON.Pole to Pole source in CoralNet
+#https://coralnet.ucsd.edu/source/1972/
 dat2<- dplyr::select (dat,Name=SourceFile,Date=CreateDate,habitat= CreatorRegion,country=CreatorCountry,locality=CreatorCity,site=CreatorAddress,strata=ObjectName,Height_cm=CreatorPostalCode,Latitude=GPSLatitude,Longitude=GPSLongitude,Camera=Model, Photographer=Creator,Water_quality=ImageDescription, Strobes=LightSource, Framing_gear_used=CreatorWorkTelephone, White_balance_card=WhiteBalance, Comments=Source)
 
 dat2$Date <- as.Date(dat2$Date,"%Y:%m:%d %H:%M:%S")
@@ -20,7 +22,7 @@ names(dat2)<-c("Name","Date","habitat","country","locality","site","strata","Hei
 #Height (cm) column only numbers allowed
 dat2$`Height (cm)` <- 60
 
-#to add info to soe column 
+#to add info to some column 
 #dat2$Strobes <- "alleviate"
 
 #Created a csv to be imported to CoralNet
