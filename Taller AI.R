d75d16f6ce66aa70ed24c17a8fed0e711707f90b
Gonzalo Bravo <gonzalobravoargentina@gmail.com>
2023-10-08 22:03:23 -0300

    update in taller AI.r

--- Taller AI.R
+++ Taller AI.R
@@ -7,33 +7,27 @@ dat <- as.data.frame(dat)# create a dataframe
 
 #create a short data.frame with the columms of interest 
 library(dplyr)
-#names used as MBON_AMP.AR source in CoralNet
 
-#https://coralnet.ucsd.edu/source/1933/
+#names used as Taller.AI source in CoralNet
+#https://coralnet.ucsd.edu/source/4417/
 dat2<- dplyr::select (dat,Name=SourceFile,Date=DateCreated,
                       country=Country,
-                      strata=Title,
                       locality=City,
-                      site=Location,
+                      state=State,
                       Latitude=GPSLatitude,Longitude=GPSLongitude,
                       Camera=Model, 
-                      Photographer=Creator)
-
+                      Photographer=Model)
 
 dat2$Date <- as.Date(dat2$Date,"%Y:%m:%d %H:%M:%S")
-
-#In case we need to change the dates 
-#library(lubridate)
-# replace the month number with a new month number
-#dat2$Date  <- update(dat2$Date , month = 4)
-dat2$habitat <- "Rocky Shore"
+dat2$habitat <- "Patio"
+dat2$site <- "CURE"
 dat2$Height_cm <- 42
-dat2$Comments <- "AMP3"
+dat2$Comments <- "Taller IA"
 dat2$Strobes <- "no"
-dat2$Framinggearused <- "25x25cm"
+dat2$Framinggearused <- "none"
 
 #Modify columns names in order to be identical to CoralNet
-names(dat2)<-c("Name","Date","country","strata","locality","site","Latitude","Longitude","Camera","Photographer","habitat","Height (cm)","Comments","Strobes","Framing gear used")   
+names(dat2)<-c("Name","Date","country","locality","state","site","Latitude","Longitude","Camera","Photographer","habitat","Height (cm)","Comments","Strobes","Framing gear used")   
 
 #Created a csv to be imported to CoralNet
 write.csv(dat2, 'metadata.csv',row.names = F)
